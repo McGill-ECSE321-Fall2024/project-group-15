59c54ab1576701c4a1a606a48c15afb24e8eded0
Jacob Kirolous Gayed <jacob.gayed@gmail.com>
2024-10-03 13:59:34 -0400

    gamestore

---
+++ gameStore/src/main/java/group15/gameStore/GameStoreApplication.java
@@ -0,0 +1,13 @@
+package group15.gameStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameStoreApplication.class, args);
+	}
+
+}
