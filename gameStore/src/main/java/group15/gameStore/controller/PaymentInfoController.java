e4eb91179ef0e6fce18c60511554c72506d04f45
jordanbuch13 <buchananjordan1306@gmail.com>
2024-11-04 19:31:43 -0500

    Update PaymentInfoController.java

--- gameStore/src/main/java/group15/gameStore/controller/PaymentInfoController.java
+++ gameStore/src/main/java/group15/gameStore/controller/PaymentInfoController.java
@@ -1,6 +1,8 @@
 package main.java.group15.gameStore.controller;
 
+import group15.gameStore.model.PaymentInfo;
 import group15.gameStore.service.PaymentInfoService;
+import main.java.group15.gameStore.dto.PaymentInfoDto;
 
 @RestController
 public class PaymentInfoController {
@@ -8,7 +10,14 @@ public class PaymentInfoController {
     @Autowired
     private PaymentInfoService paymentInfoService;
 
-    /**
-     * CreatePaymentInfo
+     /**
+     * CreatePaymentInfo: creates new payment information record
+     * @param
+     * @return the new PaymentInfo
+     * @throws IllegalArgumentException if creation request is invalid or customer is not found
      */
+    public PaymentInfoDto createPaymentInfo(@RequestBody PaymentInfoRequestDto paymentInfo){
+        PaymentInfo createdPaymentInfo = PaymentInfoService.createPaymentInfo(paymentInfo.getCardNumber,paymentInfo.getExpiryDate,paymentInfo.getCvv,paymentInfo.getBillingAddress,paymentInfo.getCustomer);
+        return new PaymentInfoDto(createdPaymentInfo);
+    }
 }
