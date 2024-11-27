9872a3ac2436e12b65909daa8e7121738202d0b8
yvehenry <yvehenry.julsain@mail.mcgill.ca>
2024-11-26 23:58:46 -0500

    Can now log in as employee

--- GameStore-Frontend/src/components/Login.vue
+++ GameStore-Frontend/src/components/Login.vue
@@ -1,47 +1,58 @@
 <template>
-    <div class="auth-page">
-      <div class="auth-container">
-        <h1>{{ isSignIn ? 'Sign In' : 'Create Account' }}</h1>
-        <form @submit.prevent="handleSubmit">
-          <div class="input-group">
-            <label for="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              v-model="email"
-              placeholder="Enter your email"
-              required
-            />
-          </div>
-          <div class="input-group">
-            <label for="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              v-model="password"
-              placeholder="Enter your password"
-              required
-            />
-          </div>
-          <div v-if="!isSignIn" class="input-group">
-            <label for="confirmPassword">Confirm Password</label>
-            <input
-              type="password"
-              id="confirmPassword"
-              v-model="confirmPassword"
-              placeholder="Confirm your password"
-              required
-            />
-          </div>
-          <button type="submit">{{ isSignIn ? 'Sign In' : 'Create Account' }}</button>
-          <div class="auth-switch">
-            <p @click="toggleAuthMode" class="switch-link">
-              {{ isSignIn ? 'Create an Account' : 'Already have an account? Sign In' }}
-            </p>
-            <p @click="handleForgotPassword" class="forgot-password-link">Forgot password?</p>
-          </div>
-        </form>
-      </div>
+    <div class="auth-container">
+      <h1 style="color: black;" v-if="isSignIn">Sign In</h1>
+      <h1 style="color: black;" v-else-if="isEmployee">Employee Sign In</h1>
+      <h1 style="color: black;" v-else>Create an Account</h1>
+
+      <form @submit.prevent="handleSubmit">
+        <div class="input-group">
+          <label for="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            v-model="email"
+            placeholder="Enter your email"
+            required
+          />
+        </div>
+        <div class="input-group">
+          <label for="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            v-model="password"
+            placeholder="Enter your password"
+            required
+          />
+        </div>
+        <div v-if="!isSignIn" class="input-group">
+          <label for="confirmPassword">Confirm Password</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            v-model="confirmPassword"
+            placeholder="Confirm your password"
+            required
+          />
+        </div>
+        <button type="submit">{{ isEmployee || isSignIn ? 'Sign In' : 'Create Account' }}</button>
+        <div class="auth-switch">
+          <p @click="toggleAuthMode" class="switch-link" v-if="isSignIn">
+            Create an Account
+          </p>
+          <p @click="toggleEmployeeMode" class="switch-link" v-else-if="isEmployee">
+            Sign in as a customer
+          </p>
+          <p @click="toggleAuthMode" class="switch-link" v-else>
+            Already have an account? Sign In
+          </p>
+          <p @click="switchToEmployeeMode" class="switch-link" v-if="!isEmployee">
+            Sign in as an Employee
+          </p>
+          <p @click="handleForgotPassword" class="forgot-password-link">Forgot password?</p>
+        </div>
+
+      </form>
     </div>
   </template>
   
@@ -51,40 +62,54 @@
     data() {
       return {
         isSignIn: true, // Tracks whether the user is signing in or creating an account
+        isEmployee: false, // Tracks whether the user is signing in as an employee
         email: "",
         password: "",
         confirmPassword: "", // Only used for account creation
       };
     },
     methods: {
-      handleSubmit() {
-        if (this.isSignIn) {
-          this.signIn();
-        } else {
-          this.createAccount();
-        }
-      },
-      signIn() {
-        console.log("Signing in with", this.email, this.password);
-        // Add logic to sign in the user, such as an API request
-        this.$router.push('/home');
-      },
-      createAccount() {
-        if (this.password !== this.confirmPassword) {
-          alert("Passwords do not match.");
-          return;
-        }
-        console.log("Creating account with", this.email, this.password);
-        // Add logic to create a new account, such as an API request
-        this.$router.push('/home');
-      },
-      toggleAuthMode() {
-        this.isSignIn = !this.isSignIn; // Toggle between sign in and create account
-      },
-      handleForgotPassword() {
-        alert("Redirecting to password recovery page...");
-        this.$router.push('/forgot-password'); // Example route for password recovery page
-      },
+  handleSubmit() {
+    if (this.isSignIn) {
+      this.signIn();
+    } else if (this.isEmployee) {
+      this.employeeSignIn();
+    } else {
+      this.createAccount();
+    }
+  },
+    signIn() {
+      console.log("Signing in with", this.email, this.password);
+      this.$router.push('/home');
+    },
+    employeeSignIn() {
+      console.log("Signing in with", this.email, this.password);
+      this.$router.push('/home');
+    },
+    createAccount() {
+      if (this.password !== this.confirmPassword) {
+        alert("Passwords do not match.");
+        return;
+      }
+      console.log("Creating account with", this.email, this.password);
+      this.$router.push('/home');
+    },
+    toggleEmployeeMode() {
+      this.isEmployee = false; // Toggle between employee and customer sign-in
+      this.isSignIn = !this.isSignIn;
+    },
+    toggleAuthMode() {
+      this.isSignIn = !this.isSignIn; // Toggle between sign-in and create account
+      this.isEmployee = false;
+    },
+    switchToEmployeeMode() {
+      this.isEmployee = true; // Switch to employee sign-in
+      this.isSignIn = false; // Ensure other modes are not active
+    },
+    handleForgotPassword() {
+      alert("Redirecting to password recovery page...");
+      this.$router.push('/forgot-password');
+    },
     },
   };
   </script>
